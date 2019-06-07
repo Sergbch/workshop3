a9d8075315adba9830d507212951468c2bc03732
Sergiy Babich <Sergbch@gmail.com>
2019-06-08 00:53:26 +0300

    Create Main.py

---
+++ km-61/Бабич/source/Main.py
@@ -0,0 +1,202 @@
+from flask import Flask, render_template, request, redirect, url_for
+from forms.search_form import SearchForm
+from dao.orm.model import *
+from dao.db import OracleDb
+from forms.user_form import UserForm
+from sqlalchemy.sql import func
+
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import json
+
+app = Flask(__name__)
+app.key = 'development key'
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def root():
+
+    return render_template('index.html')
+
+
+
+
+@app.route('/user', methods=['GET'])
+def user():
+
+    db = OracleDb()
+
+    result = db.sqlalchemy_session.query(ormUser).all()
+
+    return render_template('user.html', users = result)
+
+
+@app.route('/Document', methods=['GET'])
+def Document():
+
+    db = OracleDb()
+
+    result = db.sqlalchemy_session.query(ormDocument).all()
+
+    return render_template('Document.html', Documents = result)
+
+
+@app.route('/createDocByTemplate', methods=['GET'])
+def createDocByTemplate():
+
+    db = OracleDb()
+
+    result = db.sqlalchemy_session.query(ormUser).join(ormcreateDocByTemplate).join(ormDocument).all()
+
+    return render_template('creaateDocByTemplate.html', users = result)
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+
+    search_form = SearchForm()
+
+    if request.method=='GET':
+        return render_template('search.html', form = search_form, result=None)
+    else:
+        return render_template('search.html', form = search_form, result=search_form.get_result())
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+
+    db = OracleDb()
+
+
+    query1  = (
+                db.sqlalchemy_session.query(
+                ormUser.full_name,
+                func.count(ormcreateDocByTemplate.patch_file).label('document_count')).\
+                outerjoin(ormcreateDocByTemplate).\
+                group_by(ormUser.email,ormUser.full_name)
+               ).all()
+
+
+
+    query2 = (
+        db.sqlalchemy_session.query(
+            ormDocument.patch_file,
+            func.count(ormcreateDocByTemplate.email).label('user_count')). \
+            outerjoin(ormcreateDocByTemplate). \
+            group_by(ormDocument.patch_file)
+    ).all()
+
+
+
+
+    names, Document_counts = zip(*query1)
+    bar = go.Bar(
+        x=names,
+        y=Document_counts
+    )
+
+    Documents, user_count = zip(*query2)
+    pie = go.Pie(
+        labels=Documents,
+        values=user_count
+    )
+
+
+
+    data = {
+                "bar":[bar],
+                "pie":[pie]
+           }
+    graphsJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('dashboard.html', graphsJSON=graphsJSON)
+
+
+
+@app.route('/new_user', methods=['GET','POST'])
+def new_user():
+
+    form = UserForm()
+
+
+    if request.method == 'POST':
+        if form.valemailate() == False:
+            return render_template('user_form.html', form=form, form_name="New user", action="new_user")
+        else:
+            new_user= ormUser(
+                                id=form.id.data,
+                                full_name=form.full_name.data,
+                            )
+            db = OracleDb()
+            db.sqlalchemy_session.add(new_user)
+            db.sqlalchemy_session.commit()
+
+
+            return redirect(url_for('user'))
+
+    return render_template('user_form.html', form=form, form_name="New user", action="new_user")
+
+
+
+@app.route('/edit_user', methods=['GET','POST'])
+def edit_user():
+
+    form = UserForm()
+
+
+    if request.method == 'GET':
+
+        email =request.args.get('email')
+        db = OracleDb()
+        user = db.sqlalchemy_session.query(ormUser).filter(ormUser.email == email).one()
+
+
+        form.email.data = user.email
+        form.full_name.data = user.full_name
+        form.id.data = user.id
+
+        return render_template('user_form.html', form=form, form_name="Edit user", action="edit_user")
+
+
+    else:
+
+        if form.valemailate() == False:
+            return render_template('user_form.html', form=form, form_name="Edit user", action="edit_user")
+        else:
+            db = OracleDb()
+            # find user
+            user = db.sqlalchemy_session.query(ormUser).filter(ormUser.email == form.email.data).one()
+
+            # update fields from form data
+            user.id = form.id.data
+            user.full_name = form.full_name.data
+
+            db.sqlalchemy_session.commit()
+
+            return redirect(url_for('user'))
+
+
+
+
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+
+    email = request.form['email']
+
+    db = OracleDb()
+
+    result = db.sqlalchemy_session.query(ormUser).filter(ormUser.email ==email).one()
+
+    db.sqlalchemy_session.delete(result)
+    db.sqlalchemy_session.commit()
+
+
+    return email
+
+if __name__ == '__main__':
+    app.run(debug=True)
